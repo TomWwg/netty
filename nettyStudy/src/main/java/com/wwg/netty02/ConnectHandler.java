5be83ff73553e77c5cd606c644bd6bcb3e2b944d
TomWwg <741889962@qq.com>
2018-05-25 18:49:20 +0800

    Tom
    被回调触发的ChannelHandler;
    (当一个新的连接已经被建立时，channelActive(ChannelHandlerContext)将会被调用)

---
+++ nettyStudy/src/main/java/com/wwg/netty02/ConnectHandler.java
@@ -0,0 +1,14 @@
+package com.wwg.netty02;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by wwg on 2018/5/25.
+ */
+public class ConnectHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception{
+        System.out.println("Client " + ctx.channel().remoteAddress() + " connected");
+    }
+}
