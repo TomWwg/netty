0f0cd9c4f96da8d4eb3dd2fca646a7ae0d76f586
TomWwg <741889962@qq.com>
2018-06-04 15:12:11 +0800

    阻塞传输（OIO）
    该段代码可以处理中等数量的并发客户端，但是不能很好地伸缩到支撑成千上万的并发连入连接

---
+++ nettyStudy/src/main/java/com/wwg/netty03/PlainOioServer.java
@@ -0,0 +1,52 @@
+package com.wwg.netty03;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * 阻塞传输
+ * @author wwg
+ * @date 2018/6/4
+ */
+public class PlainOioServer {
+    public void serve(int port) throws IOException{
+        //将服务器绑定到指定窗口
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for (;;){
+                //接收连接
+                final Socket clientSocket = socket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                //创建一个新的线程来处理该连接
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream outputStream;
+                        try {
+                            outputStream = clientSocket.getOutputStream();
+                            //将消息写给已连接的客户端
+                            outputStream.write("Hi\r\n".getBytes(Charset.forName("UTF-8")));
+                            outputStream.flush();
+                            //关闭连接
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    //启动线程
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
